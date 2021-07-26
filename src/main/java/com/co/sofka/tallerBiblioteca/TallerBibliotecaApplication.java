1c5638e2d12593493a49ddda15d2ddd3f2940e81
saracarohdm <saracarohdm@gmail.com>
2021-07-26 15:22:05 -0500

    Se creo el proyecto y se enlazo a la base de datos mongoDB atlas

---
+++ src/main/java/com/co/sofka/tallerBiblioteca/TallerBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.co.sofka.tallerBiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerBibliotecaApplication.class, args);
+	}
+
+}
